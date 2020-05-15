7d328fc7b34e6e5fc57b122ba71f04d021c92587
Marco Perone <pasafama@gmail.com>
2020-05-15 15:57:27 +0200

    start defining basic datatypes and generation function

--- src/Data/FiscalCode.hs
+++ src/Data/FiscalCode.hs
@@ -1,1 +1,28 @@
-module Data.FiscalCode where
+module Data.FiscalCode
+  ( Surname(..)
+  , Name(..)
+  , Gender(..)
+  , BirthPlace(..)
+  , FiscalCode
+  , generateFiscalCode
+  )
+  where
+
+-- hourglass
+import           Time.Types
+
+newtype Surname = Surname String
+
+newtype Name = Name String
+
+data Gender
+  = Male
+  | Female
+
+newtype BirthPlace = BirthPlace String
+
+newtype FiscalCode = FiscalCode String
+
+generateFiscalCode :: Surname -> Name -> Date -> Gender -> BirthPlace -> FiscalCode
+generateFiscalCode (Surname surname) (Name name) (Date year month day) gender (BirthPlace birthPlace) =
+  _asdf
