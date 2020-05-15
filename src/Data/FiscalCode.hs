c5673cdd99fbc2e596093d5819b5434c8138cef9
Marco Perone <pasafama@gmail.com>
2020-05-15 16:09:17 +0200

    restructure FiscalCode datatype

--- src/Data/FiscalCode.hs
+++ src/Data/FiscalCode.hs
@@ -21,7 +21,15 @@ data Gender
 
 newtype BirthPlace = BirthPlace String
 
-newtype FiscalCode = FiscalCode String
+data FiscalCode = FiscalCode
+  { _fcSurname     :: String -- three letters
+  , _fcName        :: String -- three letters
+  , _fcYear        :: String -- two alphanumeric characters
+  , _fcMonth       :: String -- one letter
+  , _fcDay         :: String -- two alphanumeric characters
+  , _fcBirthPlae   :: String -- one letter and three alphanumeric characters
+  , _fcControlCode :: String -- one letter
+  }
 
 generateFiscalCode :: Surname -> Name -> Date -> Gender -> BirthPlace -> FiscalCode
 generateFiscalCode (Surname surname) (Name name) (Date year month day) gender (BirthPlace birthPlace) =
